331a2b37502c8085ea6c56d14da725c8206f5c48
igkiou <igkiou@c3a8ab28-483c-4c1d-95cd-76dd06a821cb>
2012-06-19 20:59:18 +0000

    added ellipse related tools, imdisp, and resolvent
    git-svn-id: file:///backup/rsync/home/svn/utils@25 c3a8ab28-483c-4c1d-95cd-76dd06a821cb

---
+++ EllipseFitCenter.m
@@ -0,0 +1,26 @@
+function A = EllipseFitCenter(XY, type)
+
+if (nargin < 2),
+	type = 'direct';
+end;
+
+if (strcmpi(type, 'direct')),
+	params = EllipseDirectFit(XY);
+elseif (strcmpi(type, 'taubin')),
+	params = EllipseFitByTaubin(XY);
+end;
+
+% MATLAB: ax^2 + bxy + cy^2 +dx + ey + f = 0
+% params = [a b c d e f]'
+% Mathematica: ax^2 + 2bxy + cy^2 + 2dx + 2fy + g = 0;
+a = params(1);
+b = params(2) / 2;
+c = params(3);
+d = params(4) / 2;
+f = params(5) / 2;
+g = params(6);
+
+
+A = zeros(2, 1);
+A(1) = (c * d - b * f) / (b ^ 2 - a * c);
+A(2) = (a * f - b * d) / (b ^ 2 - a * c);
